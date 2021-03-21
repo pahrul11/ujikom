170742e21c3f24d5e5d2a4159fb1475f85921e40
pahrul11 <58201509+pahrul11@users.noreply.github.com>
2021-03-21 08:28:04 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ujikom
+# test
---
+++ login.php
@@ -0,0 +1,50 @@
+
+<div class="card" style="padding: 50px; width: 40%; margin: 0 auto; margin-top: 10%; background: #66667C;" >
+<h3 style="text-align: center;" class="orange-text">Login</h3>
+	<form method="POST">
+		<div class="input_field;">
+			<label for="username"; style="color: #ffffff">Username</label>
+			<input id="username" type="text" name="username" required style="color: #ffffff">
+		</div>
+		<div class="input_field" >
+			<label for="password"; style="color: #ffffff">Passowrd</label>
+			<input id="password" type="password" name="password" required style="color: #ffffff">
+		</div>
+		<input type="submit" name="login" value="Login" class="btn orange" style="width: 100%;">
+	</form>
+</div>
+<?php 
+	if(isset($_POST['login'])){
+		$username = mysqli_real_escape_string($koneksi,$_POST['username']);
+		$password = mysqli_real_escape_string($koneksi,md5($_POST['password']));
+	
+		$sql = mysqli_query($koneksi,"SELECT * FROM masyarakat WHERE username='$username' AND password='$password' ");
+		$cek = mysqli_num_rows($sql);
+		$data = mysqli_fetch_assoc($sql);
+	
+		$sql2 = mysqli_query($koneksi,"SELECT * FROM petugas WHERE username='$username' AND password='$password' ");
+		$cek2 = mysqli_num_rows($sql2);
+		$data2 = mysqli_fetch_assoc($sql2);
+
+		if($cek>0){
+
+			session_start();
+			$_SESSION['username']=$username;
+			$_SESSION['data']=$data;
+			$_SESSION['level']='masyarakat';
+			header('location:masyarakat/');
+		}
+		elseif($cek2>0){
+			if($data2['level']=="admin"){
+				session_start();
+				$_SESSION['username']=$username;
+				$_SESSION['data']=$data2;
+				header('location:admin/');
+			}
+		}
+		else{
+			echo "<script>alert('Gagal Login Sob')</script>";
+		}
+
+	}
+ ?>
